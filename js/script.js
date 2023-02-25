c0125c775f911edbc0f8191431a70419b82b07a2
MUHAMMAD MAROOF <mmaroof487@gmail.com>
2023-02-26 00:03:23 +0530

    feat: added javascipt for login

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var x = document.getElementById('username');
 var y = document.getElementById('password');
 var z = document.getElementById('submit');
-var a = document.getElementById('image');
+var a = document.getElementById('loadingscreen');
 
 var name = "muhammad_maroof1";
 var pass = "ilu>c8csab";
@@ -9,6 +9,8 @@ var pass = "ilu>c8csab";
 z.onclick = function(){
     if ( x.value == name && y.value == pass){
         console.log("Login Succesful");
+        a.style.display = "block";
+        return false;
     }else{
         console.log("Login Failed");
     }
